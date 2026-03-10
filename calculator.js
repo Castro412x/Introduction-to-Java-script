398c8e06d6fb6ba6f819ef4c70ab612af4c1dc97
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-10 22:27:17 +0100

    Add Calculator

---
+++ calculator.js
@@ -0,0 +1,9 @@
+const numA = 48
+const numB = 6
+
+console.log( numA + numB )
+console.log( numA - numB )
+console.log( numA * numB )
+console.log( numA / numB )
+console.log( numA % numB )
+console.log( numA**2 )
