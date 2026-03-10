d481507a1ff29f7a9459d0af963afd3b6bf7f092
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-10 22:26:13 +0100

    Add datatype Explorer

---
+++ datatype.js
@@ -0,0 +1,20 @@
+const myString = "Emmanuel"
+const myNumber = 10
+const myBoolean = true
+const myNull = null
+const myUndefined = undefined
+console.log(
+    myString, typeof myString
+)
+console.log(
+    myNumber, typeof myNumber
+)
+console.log(
+    myBoolean, typeof myBoolean
+)
+console.log(
+    myNull, typeof myNull
+)
+console.log(
+    myUndefined, typeof myUndefined
+)
