e6fa5b68d0792a1972c99c2877f43d49e5de615c
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-10 22:37:42 +0100

    Add tempertraure Converter

---
+++ temperature.js
@@ -0,0 +1,11 @@
+let firstcelsius = 100
+let firstfahrenheit = (firstcelsius * 9/5) + 32
+console.log(`${firstcelsius} degree Celsius is equal to ${firstfahrenheit} degree fahrenheit`)
+
+let secondcelsius = 0
+let secondfahrenheit = (secondcelsius * 9/5) + 32
+console.log(`${secondcelsius} degree Celsius is equal to ${secondfahrenheit} degree fahrenheit`)
+
+let thirdcelsius = 37
+let thirdfahrenheit = (thirdcelsius * 9/5) + 32
+console.log(`${thirdcelsius} degree Celsius is equal to ${thirdfahrenheit} degree fahrenheit`)
