868de6b5353025c78f9d538e844966ba7d062462
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-10 22:20:16 +0100

    Add Personal

---
+++ personal.js
@@ -0,0 +1,6 @@
+const fullname = "Nonyerem Emmanuel"
+const age = 10
+const city = "Aba, Abia State"
+const favSubject = "Computer Studies."
+
+console.log(`My name is ${fullname}. I am ${age} years old. I live in ${city}, and my favourite subject is ${favSubject}`)
