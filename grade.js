79f8331dbe61e9d805507ab1010aca1ea32f9d63
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-10 22:39:21 +0100

    Add Grade Classifier

---
+++ grade.js
@@ -0,0 +1,16 @@
+const grade = 43
+    if(grade >= 90 ){
+        console.log("Grade A")
+    }
+    else if(grade >= 80){
+        console.log("Grade B")
+    }
+    else if (grade >= 70){
+        console.log("Grade C")
+    }
+    else if (grade >= 60){
+        console.log("Grade F")
+    }
+    else{
+        console.log("Failed")
+    }
