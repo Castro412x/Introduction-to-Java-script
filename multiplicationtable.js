b69bfa6b47b943660a9f20fa3479c94a57e82b6f
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-10 23:02:50 +0100

    Add multiplication table

--- multiplicationtable.js
+++ multiplicationtable.js
@@ -1,1 +1,8 @@
-const
+let base = 7;
+for (let i = 1; i <= 12; i++ ){
+    console.log(`${base} * ${i} = ${base * i }`)
+}
+ base = 9;
+ for (let i = 1; i <=12; i++ ){
+    console.log(`${base} * ${i} = ${base * i }`)
+ }
